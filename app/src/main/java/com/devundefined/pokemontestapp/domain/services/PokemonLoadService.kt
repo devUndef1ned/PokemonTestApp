370da62dc1d4babc380d6e4c4fbefd7abf820ca6
Oleg Kuzmin <o.kuzmin@dodopizza.com>
2019-05-30 23:58:14 +0300

    LoadPokemonService

--- app/src/main/java/com/devundefined/pokemontestapp/domain/services/PokemonLoadService.kt
+++ app/src/main/java/com/devundefined/pokemontestapp/domain/services/PokemonLoadService.kt
@@ -1,7 +1,36 @@
 package com.devundefined.pokemontestapp.domain.services
 
 import com.devundefined.pokemontestapp.domain.models.Pokemon
+import com.devundefined.pokemontestapp.infrastructure.backend.PokemonInfoApi
+import com.devundefined.pokemontestapp.infrastructure.backend.dto.PokemonDto
+import com.devundefined.pokemontestapp.infrastructure.backend.dto.PokemonInfoDto
 
 interface PokemonLoadService {
     fun loadPokemons(): List<Pokemon>
+}
+
+class PokemonLoadServiceImpl(private val pokemonApi: PokemonInfoApi) : PokemonLoadService {
+
+    val getId: (PokemonInfoDto) -> Int = { pokemonInfoDto ->
+        pokemonInfoDto.url.let { string ->
+            val indexOfLastDash = string.indexOfLast { it == '/' }
+            val cutString = string.substring(0, indexOfLastDash - 1)
+            val indexOfPrelastDash = cutString.indexOfLast { it == '/' }
+            cutString.substring(indexOfPrelastDash, cutString.length - 1).toInt()
+        }
+    }
+
+    val toModel: (PokemonDto) -> Pokemon = { pokemonDto ->
+        with(pokemonDto) { Pokemon(id, name, sprite.url, height, weight, listOf(*types.map { it.typeName.name }.toTypedArray())) }
+
+    }
+
+    override fun loadPokemons(): List<Pokemon> {
+        val result = arrayListOf<Pokemon>()
+        pokemonApi.getPokemonInfoList().results.map(getId).forEach { id ->
+            result.add(toModel(pokemonApi.getPokemon(id)))
+        }
+
+        return result
+    }
 }
